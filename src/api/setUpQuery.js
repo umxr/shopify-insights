f46215cdcab3ea2cca1fbf97f73ece356466f46f
Umar Gora <umarg1997@gmail.com>
2020-11-19 10:58:32 +0000

    Update setUpQuery.js

--- src/api/setUpQuery.js
+++ src/api/setUpQuery.js
@@ -1,13 +1,15 @@
 const setUpQuery = (page) => {
-  const api = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyCAKk7B5Mo-_z4G9LL_PtbbpqTot8JwV_I";
+  const api = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed";
   const parameters = {
     url: encodeURIComponent(page),
+    key: "AIzaSyCAKk7B5Mo-_z4G9LL_PtbbpqTot8JwV_I"
   };
   let query = `${api}?`;
   let key;
   for (key in parameters) {
     query += `${key}=${parameters[key]}`;
   }
+  console.log(query);
   return query;
 };
 
